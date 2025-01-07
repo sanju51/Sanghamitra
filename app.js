795ccd69326bfd31d2c6a58cd24c96d6f33a4238
sanju51 <sanjanabu47@gmail.com>
2025-01-08 00:15:46 +0530

    jkl

--- app.js
+++ app.js
@@ -1,99 +1,97 @@
-const express=require("express");
-const app = express(); 
-const mongoose =require("mongoose");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
-const methodOverride=require("method-override");
-const ejsMate=require("ejs-mate");
+const methodOverride = require("method-override");
+const ejsMate= require("ejs-mate");
 
-const MONGO_URL= "mongodb://127.0.0.1:27017/Sanghamitra";
+const MONGO_URL = "mongodb://127.0.0.1:27017/Sanghamitra";
 
 main()
-.then(() => {
-    console.log("connected to DB")
-})
-.catch((err) => {
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
-async function main(){
-    await mongoose.connect(MONGO_URL);
+async function main() {
+  await mongoose.connect(MONGO_URL);
 }
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs",ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
-app.get("/", (req,res) => {
-    res.send("Hi, I am root");    
+app.get("/", (req, res) => {
+  res.send("Hi, I am root");
 });
 
 //Index Route
-app.get("/listings", async(req,res) =>{
-    const allListings=await Listing.find({});
-    res.render("Listings/index.ejs",{ allListings});
-    });
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
+});
 
-//new route
-app.get("/listings/new", (req,res)=>{
-    res.render("listings/new.ejs");
+//New Route
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
 });
 
 //Show Route
-app.get("/listings/:id" , async (req,res)=>{
-    let{id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/show.ejs", {listing});
-
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
 });
 
-//create route
-app.post("/listings", async (req,res) => {  
-    const newListing = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
+//Create Route
+app.post("/listings", async (req, res) => {
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
 });
 
-//edit route
-app.get("/listings/:id/edit", async(req,res) => {
-    let{id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs", {listing});
+//Edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
 });
 
-//update route
-app.put("/listings/:id", async(req,res) =>{
-    let {id}=req.params;
-    await Listing.findByIdAndUpdate(id, { ...req.body.listing});
-    res.redirect(`/listings/${id}`);
-
+//Update Route
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
 });
 
-//DELETE route
-app.delete("/listings/:id", async(req,res) => {
-    let{id} = req.params;
-    let deletedListing =await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    res.redirect("/listings");
+//Delete Route
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");
 });
 
-/* app.get("/testlisting" , async (req,res)  =>{
-    let sampleListing=new Listing({
-        title:"My new Villa",
-        description: "By the beach",
-        price: 1200,
-        location : "Goa",
-        country: "India",
-    });
+// app.get("/testListing", async (req, res) => {
+//   let sampleListing = new Listing({
+//     title: "My New Villa",
+//     description: "By the beach",
+//     price: 1200,
+//     location: "Calangute, Goa",
+//     country: "India",
+//   });
 
-    await sampleListing.save();
-    console.log("sample was saved");
-    res.send("succesfull testing");
-});*/
+//   await sampleListing.save();
+//   console.log("sample was saved");
+//   res.send("successful testing");
+// });
 
-app.listen(8080 , () => {
-    console.log("server is running on port 8080");
+app.listen(8080, () => {
+  console.log("server is listening to port 8080");
 });
--- models/listing.js
+++ models/listing.js
@@ -1,26 +1,26 @@
+
 const mongoose = require("mongoose");
-const Schema=mongoose.Schema;
+const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
-    title: {
-        type: String,
-        required: true,
-    },
-    description: String,
-    image: {
-        type: String,
-        default:
-          "https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?ixlib=rb-4.0.3&q=80&w=800&fit=crop&auto=format",
-        set: (v) =>
-          v === "  "
-            ? "https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?ixlib=rb-4.0.3&q=80&w=800&fit=crop&auto=format"
-            : v,
-      },
-    price: Number,
-    location: String,
-    country: String,
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    type: String,
+    default:
+      "https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGxha2V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60",
+    set: (v) =>
+      v === ""
+        ? "https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGxha2V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60"
+        : v,
+  },
+  price: Number,
+  location: String,
+  country: String,
 });
 
-
-const Listing = mongoose.model("Listing", listingSchema)
-module.exports=Listing;
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
