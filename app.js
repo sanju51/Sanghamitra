c108b5a3b60afe0cff4e4aab2de975c5a117e761
sanju51 <sanjanabu47@gmail.com>
2025-02-04 01:48:23 +0530

    neww

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const methodOverride = require("method-override");
 const ejsMate= require("ejs-mate");
 const wrapAsync =require("./utils/wrapAsync.js");
 const ExpressError =require("./utils/ExpressError.js");
-const {listingSchema}=require("./schema.js");
+const {listingSchema , reviewSchema}=require("./schema.js");
+const Review = require("./models/review.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/Sanghamitra";
 
@@ -45,6 +46,17 @@ const validateListing=(req,res,next)=>{
   }
 };
 
+const validateReview=(req,res,next)=>{
+  let {error}=reviewSchema.validate(req.body);
+  
+  if(error){
+  let errMsg=error.details.map((el)=>el.message).join(",");
+   throw new ExpressError(400,errMsg);
+  }else{
+    next();
+  }
+};
+
 //Index Route
 app.get("/listings", wrapAsync(async (req, res) => {
   const allListings = await Listing.find({});
@@ -59,7 +71,7 @@ app.get("/listings/new", (req, res) => {
 //Show Route
 app.get("/listings/:id", wrapAsync(async (req, res) => {
   let { id } = req.params;
-  const listing = await Listing.findById(id);
+  const listing = await Listing.findById(id).populate("reviews");
   res.render("listings/show.ejs", { listing });
 }));
 
@@ -96,6 +108,30 @@ app.delete("/listings/:id", wrapAsync(async (req, res) => {
   console.log(deletedListing);
   res.redirect("/listings");
 }));
+
+//Reviews Post route
+app.post("/listings/:id/reviews",validateReview, wrapAsync(async(req,res) =>{
+  let listing=await Listing.findById(req.params.id);
+  let newReview = new Review(req.body.review);
+
+  listing.reviews.push(newReview);
+
+  await newReview.save();
+  await listing.save();
+
+  res.redirect(`/listings/${listing._id}`);
+
+}));
+
+//Delete review route
+app.delete("/listings/:id/reviews/:reviewId" ,wrapAsync(async (req,res)=>
+{
+  let{id,reviewId}=req.params;
+  await Review.findById(reviewId);
+
+  res.redirecr(`/listings/${id}`);
+}
+));
 
 // app.get("/testListing", async (req, res) => {
 //   let sampleListing = new Listing({
