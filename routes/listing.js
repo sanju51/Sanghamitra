bcc345227e7d1be8dfe3d85303a492d298b3ffe3
sanju51 <sanjanabu47@gmail.com>
2025-02-19 23:49:23 +0530

    Edit Listing image

--- routes/listing.js
+++ routes/listing.js
@@ -21,6 +21,7 @@ router.route("/")
   router.route("/:id")
   .get(wrapAsync(listingController.showListing))
   .put(isLoggedIn,isOwner,
+    upload.single('listing[image]'),
     validateListing,
     wrapAsync( listingController.updateListing))
     .delete(isLoggedIn,isOwner,
